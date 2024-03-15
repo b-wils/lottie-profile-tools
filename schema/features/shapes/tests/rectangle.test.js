161080bb11d466b63fd9daa908b7c968167deaf7
Brandon Wilson <bwils@google.com>
2024-03-15 15:49:47 -0400

    Add profile definition and check.
    Note this required moving the full lottie structure into the profile.
    This is because each check needs access to the object root so it can
    check the profile setting as well.

--- schema/features/shapes/tests/rectangle.test.js
+++ schema/features/shapes/tests/rectangle.test.js
@@ -11,32 +11,57 @@ const ajv = new Ajv({
 
 require("ajv-errors")(ajv);
 
-// ajv.addKeyword({
-//   keyword: "errorMessage",
-//   type: "object",
-//   schemaType: "string",
-// });
 
 describe('Rectangle Validation', () => {
   let validate;
-
+  let profile;
   beforeAll(async () => {
     const bundledSchema = await makeSchema();
     validate = ajv.compile(bundledSchema);
   });
 
-  test('fails if shape is a rectangle', async () => {
-    const valid = validate(rectLottie);
-    expect(valid).toBe(false);
-    // const errorMessages = getUserErrorMessages(validate);
-    const errorMessages = validate.errors.filter(error => error.keyword === 'errorMessage').map(error => error.message);
+  describe('rectangle enabled', () => {
+    beforeAll(() => {
+      profile = makeAllEnabledProfile();
+    });
 
-    expect(errorMessages.length).toBe(1);
-    expect(errorMessages[0]).toBe('Rectangle not supported by current profile');
+    test('passes if shape is a rectangle', async () => {
+      const valid = validate({lottie: rectLottie, profile: makeAllEnabledProfile()});
+      expect(valid).toBe(true);
+    });
+
+    test('passes if shape is an ellipse', async () => {
+      const valid = validate({lottie: ellipseLottie});
+      expect(valid).toBe(true);
+    });
   });
 
-  test('passes if shape is an ellipse', async () => {
-    const valid = validate(ellipseLottie);
-    expect(valid).toBe(true);
+  describe('rectangle disabled', () => {
+    beforeAll(() => {
+      profile = makeAllEnabledProfile();
+      profile.shapes.rectangle = false;
+    });
+
+    test('fails if shape is a rectangle', async () => {
+      const valid = validate({lottie: rectLottie});
+      expect(valid).toBe(false);
+      const errorMessages = validate.errors.filter(error => error.keyword === 'errorMessage').map(error => error.message);
+  
+      expect(errorMessages.length).toBe(1);
+      expect(errorMessages[0]).toBe('Rectangle not supported by current profile');
+    });
+  
+    test('passes if shape is an ellipse', async () => {
+      const valid = validate({lottie: ellipseLottie});
+      expect(valid).toBe(true);
+    });
   });
 });
+
+function makeAllEnabledProfile() {
+  return {
+    shapes: {
+      rectangle: true,
+    }
+  }
+}
